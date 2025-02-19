824d6c16be9c680467958cad293e3643db991109
Simonkhawam <133916861+Simonkhawam@users.noreply.github.com>
2025-02-19 20:54:44 +0200

    settings modified

--- My_Final_Project/settings.py
+++ My_Final_Project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gzq@w-mib$at-$g&0(y#1@wjbu7uz%(^-@-zxp=o_oa5nrqjwg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['raahub-16a5dd72bf90.herokuapp.com', 'localhost']
 
 
 # Application definition
